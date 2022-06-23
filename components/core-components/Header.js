934eb6b24b3d96ee95761667ba9591d4c9af8e76
ThimbleBGYC <99198921+ThimbleBGYC@users.noreply.github.com>
2022-06-23 16:37:52 -0400

    Update Header.js

--- components/core-components/Header.js
+++ components/core-components/Header.js
@@ -13,9 +13,6 @@ import { Stack } from "@mui/material";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
-export const navLinks = [
-  { title: 'Home', path: 'https://boardgameyawnclub.xyz/'},
-];
 
 const Header = () => {
   return (
